82b81d51b7b81b1ee379b15a26eae11dbb0e22c2
Jim Holmberg <hasenpfad1@gmail.com>
2019-08-20 14:40:01 -0500

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+    
+INSERT INTO burgers(burger_name, devoured)
+	values('Quinoa Jackie Burger', false),
+    ('Bacon Cheese Christian Burger', true),
+    ('Holmburger', false);
+    
+    select * from burgers;
